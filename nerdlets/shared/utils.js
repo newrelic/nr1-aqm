43d42a4f0de54e706b4960cb1d411deb2d3216be
khpeet <kpeet@newrelic.com>
2025-10-23 15:18:29 -0400

    chore: remove unused uuid util

--- nerdlets/shared/utils.js
+++ nerdlets/shared/utils.js
@@ -1,14 +1,6 @@
 import { NerdGraphQuery } from 'nr1';
 import { EXCLUDED_ENTITY_TYPES, RECOMMENDATIONS } from './constants';
 
-export const uuid = () =>
-  crypto?.randomUUID?.() ||
-  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = (Math.random() * 16) | 0;
-    const v = c === 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-
 export const chunkData = (d, size) => {
   return new Promise((resolve) => {
     let chunked = d.reduceRight(
